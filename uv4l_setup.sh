8d5536afb69291a1e736bd12c909099a25b24ba7
Your Name <you@example.com>
2015-10-02 09:58:46 +0000

    add info

--- uv4l_setup.sh
+++ uv4l_setup.sh
@@ -7,5 +7,5 @@ sudo mv ./setup/sources.list /etc/apt/
 
 sudo apt-get update
 
-sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream
+sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-webrtc
 
