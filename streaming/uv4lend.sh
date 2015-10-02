ed26f74ff870b8ad391b4a3f719427a189e9dd2e
Your Name <you@example.com>
2015-10-02 09:52:30 +0000

    commit

---
+++ streaming/uv4lend.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+sudo pkill uv4l
+
+
---
+++ uv4l_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc
+
+sudo apt-key add ./lrkey.asc
+
+sudo mv ./setup/sources.list /etc/apt/
+
+sudo apt-get update
+
+sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream
+
