d2ba9799f4aab186c7c2cfefee4a9075b9861ca9
Your Name <you@example.com>
2015-10-02 12:05:41 +0000

    update

--- uv4l_setup.sh
+++ uv4l_setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc
 
-sudo apt-key add ./lrkey.asc
+apt-key add ./lrkey.asc
 
-sudo echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/sources.list
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/sources.list
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-webrtc
+apt-get install uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-webrtc
 
-echo 'alias stream="~/uv4l_setup/streaming/uv4lconfig.sh"' >> ~/.bashrc
-echo 'alias strend="~/uv4l_setup/streaming/uv4lend.sh"' >> ~/.bashrc
+echo 'alias stream="~/uv4l_setup/streaming/uv4lconfig.sh"' >> /home/pi/.bashrc
+echo 'alias strend="~/uv4l_setup/streaming/uv4lend.sh"' >> /home/pi/.bashrc
 
